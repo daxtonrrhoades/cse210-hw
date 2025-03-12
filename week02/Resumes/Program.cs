da6ee0c1c92a5477a329c2aecf8575f3809a53a0
DaxtonRRhoades <daxtonrrhoades@gmail.com>
2025-03-12 14:49:44 -0600

    Created Week 2 Resume assignment

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Teller";
+        job1._company = "America First Credit Union";
+        job1._startYear = 2023;
+        job1._endYear = 2028;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales Associate";
+        job2._company = "Old Navy";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Daxton Rhoades";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
